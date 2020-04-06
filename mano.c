1021b18e3002b785d66ab69c8d22fd1d234ac814
Bruno Alexandre <bruno.in.dk@gmail.com>
2020-04-06 22:51:49 +0200

    refactored variables; added coments;

--- mano.c
+++ mano.c
@@ -4,60 +4,92 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+#define MIN 0
+#define MAX 32
 
-void process_single_tree(char *row, int level, int n)
+/**
+ * Validates if the interaction number is between the acceptable range
+ * @param int lines to process
+ * @return nothing
+ */
+void validateInteractions(int interactions)
 {
-    // printf("process_tree(row: %s, level: %d, n: %d)\n", row, level, n);
-    
-    int pid;
-    int status;
-
-    if (n > level)
+    if (interactions < MIN || interactions > MAX)
     {
-        exit(0);
-    }
-    else
-    {
-        pid = fork();
-        switch (pid)
-        {
-        case -1:
-            printf("fork failed\n");
-            break;
-        case 0:
-            printf("Process %s%d %d, from %d\n", row, n, getpid(), getppid());
-            n += 1;
-            process_single_tree(row, level, n);
-            break;
-
-        default:
-            break;
-        }
-        
-        wait(&status);
+        printf("Level must be between %d and %d", MIN, MAX);
+        exit(1);
     }
 }
 
-void process_double_tree(int level, int n)
-{
-    // printf("process_tree_2(level: %d, n: %d)\n", level, n);
+/**
+ * Validates if there are enough arguments
+ * @param int number of arguments (argc)
+ * @return nothing
+ */
+void validateArguments(int arguments) {
+    if (arguments < 2)
+    {
+        printf("A decimal argument must be declared");
+        exit(1);
+    }
+}
 
-    int i;
-    int index = 2;
+/**
+ * Processes a single fork() and his child
+ * @param char the column Letter that the column belongs
+ * @param int the max interactions to process
+ * @param int the current row id
+ * @return nothing
+ */
+void process_single_tree(char *colLetter, int maxInteractions, int currentRow)
+{
     int pid;
     int status;
-    char *row;
+
+    if (currentRow > maxInteractions)
+    {
+        exit(0);
+    }
+
+    pid = fork();
+    switch (pid)
+    {
+    case -1:
+        printf("fork failed\n");
+        break;
+    case 0:
+        printf("Process %s%d %d, from %d\n", colLetter, currentRow, getpid(), getppid());
+        currentRow += 1;
+            // process 1 fork and his child
+        process_single_tree(colLetter, maxInteractions, currentRow);
+        break;
+
+    default:
+        break;
+    }
+
+    wait(&status);
+}
+
+/**
+ * Processes double fork() and their childs
+ * @param int the max interactions to process
+ * @param int the current row id
+ * @return nothing
+ */
+void process_double_tree(int maxInteractions, int currentRow)
+{
+    int i;
+    int index = 2; // process twice
+    int pid;
+    int status;
+    char *colLetter; // column letter
 
     for (i = 1; i <= index; i++)
     {
-        row = i == 1 ? "A" : "B";
-        if (n > 1)
-        {
-            // printf("fork exit(0)\n");
+        colLetter = i == 1 ? "A" : "B";
+        if (currentRow > 1)
             exit(0);
-        } else {
-            // printf("for(i: %d, index: %d, n: %d, row: %s)\n", i, index, n, row);
-        }
 
         pid = fork();
         switch (pid)
@@ -66,10 +98,10 @@ void process_double_tree(int level, int n)
             printf("fork failed\n");
             break;
         case 0:
-            // printf("Process %s%d (%d/%d) %d, from %d\n", row, n, level, index, getpid(), getppid());
-            printf("Process %s%d %d, from %d\n", row, n, getpid(), getppid());
-            n += 1;
-            process_single_tree(row, level, n);
+            printf("Process %s%d %d, from %d\n", colLetter, currentRow, getpid(), getppid());
+            currentRow += 1;
+            // process 1 fork and his child
+            process_single_tree(colLetter, maxInteractions, currentRow);
             break;
 
         default:
@@ -82,20 +114,26 @@ void process_double_tree(int level, int n)
     }
 }
 
-// based on https://stackoverflow.com/questions/46250145
 int main(int argc, char *argv[])
 {
-    int level = 0;
-    int n = 1;
+    int interactions;
+    int currentRow = 1;
 
-    // TODO: check if "argv"'s exist
-    level = atoi(argv[1]);
-    // TODO: check if "n" is between MIN and MAX
+    // check if "argv[1]" is valid
+    validateArguments(argc);
 
-    printf("Inverted V process tree with n=%d\n", level);
+    interactions = atoi(argv[1]);
+
+    // check if level is between MIN and MAX
+    validateInteractions(interactions);
+
+    printf("Inverted V process tree with n=%d\n", interactions);
     printf("Process AB has PID=%d\n", getpid());
-    
-    if(level > 0) {
-        process_double_tree(level, n);
+
+    // there are levels to process
+    if (interactions > 0)
+    {
+        // create 2 forks and process each child
+        process_double_tree(interactions, currentRow);
     }
 }
