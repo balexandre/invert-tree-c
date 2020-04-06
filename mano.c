e313b055422fa3aaa4a65d882ef94fc3a794e0b9
Bruno Alexandre <bma@sitecampaign.com>
2020-04-06 16:57:57 +0200

    in English :)

--- mano.c
+++ mano.c
@@ -4,39 +4,48 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-pid_t gera_processo(char* tipo, int i) {
-    pid_t pid = fork();
-
-    if (pid < 0) { perror("Erro na funcao fork()"); exit(1); }
-    // if (pid > 0) printf("Processo %s%d tem PID=%d e PPID=%d\n", tipo, i, (int)getpid(), (int)getppid());
-
-    return pid;
-}
-
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     pid_t pidA = 0, pidB = 0;
     int n, i, parentIdA, parentIdB, status;
 
-    // TODO: verificar se existem "argv"'s
+    // TODO: check if "argv"'s exist
     n = atoi(argv[1]);
-    // TODO: verificar se "n" está compreendido entre MIN e MAX
+    // TODO: check if "n" is between MIN and MAX
 
     parentIdA = parentIdB = (int)getpid();
-    printf("\nCadeia de processos em V invertido com n=%d\n", n);
-    printf("Processo AB tem PID=%d\n", parentIdA);
+    printf("\Inverted V tree with n=%d\n", n);
+    printf("Process AB has PID=%d\n", parentIdA);
 
-    if(n > 0) {
-        for (i = 1; i <= n; i += 1) {
+    if (n > 0)
+    {
+        for (i = 1; i <= n; i += 1)
+        {
             pidA = fork();
 
-            if((int)pidA > 0) {
+            if ((int)pidA > 0)
+            {
                 // printf("> %d | %d | %d | %d\n", i, (int)pidA, (int)getpid(), (int)getppid());
-                // pidB = gera_processo("B", i, parentIdB);
-                printf("Processo A%d tem PID=%d e PPID=%d\n", i, (int)pidA, (int)getpid());
-                break; // nao deixa imprimir filhos
-            } else {
-                // printf("> FILHO %d | %d | %d | %d\n", i, (int)pidA, (int)getpid(), (int)getppid);
-                waitpid(pidA, &status, 0);
+                printf("Process A%d has PID=%d and PPID=%d\n", i, (int)pidA, (int)getpid());
+                break; // don't print child
+            }
+            else
+            {
+                //waitpid(pidA, &status, 0);
+
+                pidB = fork();
+
+                if ((int)pidB > 0)
+                {
+                    // printf("> %d | %d | %d | %d\n", i, (int)pidA, (int)getpid(), (int)getppid());
+                    printf("Process B%d has PID=%d and PPID=%d\n", i, (int)pidB, i == 1 ? (int)getppid() : (int)getppid());
+                    break; // don't print child
+                }
+                else
+                {
+                    // printf("> %d | %d | %d | %d\n", i, (int)pidA, (int)getpid(), (int)getppid);
+                    waitpid(pidB, &status, 0);
+                }
             }
         }
     }
--- readme.md
+++ readme.md
@@ -1,2 +1,7 @@
 Simple project to get `fork()` tree
 
+to run
+
+``bash
+gcc -Wall -o mano mano.c && ./mano 5
+```
